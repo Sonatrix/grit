55421c03e08362a3ed67956984dbbbbcf94c9d40
Sonatrix <praveenkumarverma121@gmail.com>
2018-07-06 01:17:43 +0530

    Add products via category

--- locator/urls.py
+++ locator/urls.py
@@ -6,4 +6,5 @@ app_name = 'locator'
 urlpatterns = [
     path('', home.index, name = 'home'),
     path('products/<slug:slug>/', home.product_details, name='product_detail'),
+    path('<slug:slug>/', home.product_category, name='category_products'),
 ]
--- locator/views/home.py
+++ locator/views/home.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from locator.models import Product, Category
 
 def index(request):
@@ -11,4 +11,8 @@ def product_details(request, slug):
 	categories = Category.objects.all()
 	return render(request, 'product/product_details.html', {"product": product, "categories": categories})
 
-    
+def product_category(request, slug):
+    category = Category.objects.get(slug=slug)
+    products = get_list_or_404(Product, category_id=category.id)
+    categories = Category.objects.all()
+    return render(request, 'product/product_category.html', {"products": products, "categories": categories, "name": slug})
