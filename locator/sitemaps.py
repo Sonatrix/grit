99e16eba685c3e2117dfcf8e73a527aa658a8b5b
Sonatrix <praveenkumarverma121@gmail.com>
2018-08-04 14:45:47 +0530

    Better sitemap url

--- locator/sitemaps.py
+++ locator/sitemaps.py
@@ -2,8 +2,9 @@ from django.contrib.sitemaps import Sitemap
 from locator.models import Product, Category
 
 class ProductSitemap(Sitemap):    
-    changefreq = "weekly"
-    priority = 0.9
+    changefreq = "daily"
+    priority = 0.5
+    protocol = "https"
 
     def items(self):
         return Product.objects.all()
