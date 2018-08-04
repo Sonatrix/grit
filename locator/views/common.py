e403c2ccf7acfb69a1191b1462533855dd2b4c06
Sonatrix <praveenkumarverma121@gmail.com>
2018-08-04 22:13:06 +0530

    Fix footer text

--- locator/views/common.py
+++ locator/views/common.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
 from locator.models import Product, Category
 
-
 def contact(request):
-	categories = Category.objects.all()
-	
+	categories = Category.objects.all()[:5]
+
 	return render(request, 'locator/common/contact_us.html', {"categories": categories})
