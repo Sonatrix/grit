2358d00f121dfc4b91e9bf093d078fade7b3d32c
Sonatrix <praveenkumarverma121@gmail.com>
2018-08-09 01:29:22 +0530

    Add url image, sender field

--- blog/models.py
+++ blog/models.py
@@ -4,35 +4,48 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from django.urls import reverse
 
+
 class PublishedManager(models.Manager):
-	def get_queryset(self):
-		return super().get_queryset().filter(status='published')
-		
+
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+
 class Post(models.Model):
-	STATUS_CHOICES = (('draft', 'Draft'),('published', 'Published'))
-	title = models.CharField(max_length=250)
-	slug = models.SlugField(max_length=250, unique_for_date='publish')
-	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'))
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
 
-	body = models.TextField()
-	publish = models.DateTimeField(default=timezone.now)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
-	tags = ArrayField(models.CharField(max_length=250), blank=True, null=True)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    tags = ArrayField(models.CharField(max_length=250), blank=True, null=True)
+    external_url = models.URLField(default="")
 
-	objects = models.Manager()
-	published = PublishedManager()
-	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
-	# need to add external url and images link
-	category = models.ForeignKey('locator.Category', on_delete=models.CASCADE)
+    objects = models.Manager()
+    published = PublishedManager()
 
-	class Meta:
-		ordering = ('-publish',)
-		db_table = 'post'
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft')
 
-	def __str__(self):
-		return self.title
-    
-	def get_absolute_url(self):
-		return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+    category = models.ForeignKey('locator.Category', on_delete=models.CASCADE)
 
+    images = ArrayField(
+        models.URLField(max_length=255),
+        size=10,
+        max_length=(255 * 11)  # 6 * 10 character nominals, plus commas
+    )
+    sender = models.CharField(max_length=255, default="shoppstar")
+
+    class Meta:
+        ordering = ('-publish',)
+        db_table = 'post'
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
