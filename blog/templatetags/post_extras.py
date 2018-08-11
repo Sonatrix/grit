a189aec6caba8af6ec0642d6995d3e02c50dc185
Sonatrix <praveenkumarverma121@gmail.com>
2018-08-11 15:38:18 +0530

    Add template tags

---
+++ blog/templatetags/post_extras.py
@@ -0,0 +1,14 @@
+from django import template
+from blog.models import Post
+
+register = template.Library()
+
+@register.inclusion_tag("blog/post/latest_posts.html")
+def show_latest_posts(count=5):
+	latest_posts = Post.published.order_by("-publish")[:count]
+	return {"latest_posts": latest_posts}
+
+@register.inclusion_tag("blog/post/featured_posts.html")
+def show_featured_posts(count=5):
+	featured_posts = Post.published.exclude(is_featured=False).order_by("-publish")[:count]
+	return {"featured_posts": featured_posts}
